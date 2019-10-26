6706eb89687d788ad72c1772412ee48e58289799
linylmfos <iqianbaidu@163.com>
2019-10-26 10:14:09 +0800

    2nd commit

---
+++ vueExe/vuedemo/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"  mysel="self shu xing" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  mounted() {
+    /* eslint-disable */
+    console.log('attrs属性', this.$attrs)
+    /* eslint-enable */
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
